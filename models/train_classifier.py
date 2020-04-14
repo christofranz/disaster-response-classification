b1d26563904e9a58d1cf39d86c9ddedbf9fc665c
Franzke <franzk1@emea.corpdir.net>
2020-04-14 15:22:14 +0200

    Add initial functionality to train_classifier.py

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,123 @@
 import sys
+import re
 
+import numpy as np
+import pandas as pd
+from sqlalchemy import create_engine
+
+import nltk
+import pickle
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize # make sure documented in readme how to install
+from nltk.stem.porter import PorterStemmer
+from nltk.stem import WordNetLemmatizer
+
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import classification_report, accuracy_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.multioutput import MultiOutputClassifier
 
 def load_data(database_filepath):
-    pass
+    """
+    Load database and split into feature and response variables.
 
+    :param database_filepath: Path to the database containing cleaned data
+    :return: X - Pandas Series of the messages
+             Y - Pandas Dataframe with the categories of the messages
+             category_names: List of strings of the categories
+    """
+    engine = create_engine('sqlite:///' + database_filepath)
+    df = pd.read_sql_table("DisasterResponses", engine)
+    # feature variable
+    X = df['message']
+    # response variable
+    Y = df.iloc[:,4:]
+    # category names
+    category_names = list(Y.columns)
+
+    return X, Y, category_names
 
 def tokenize(text):
-    pass
+    """
+    Create tokens from the text.
+
+    :param text: String of the text
+    :return: List of tokens extracted from the text
+    """
+    # remove everything not alpha-numerical in the text
+    # and normalize the text to lower case
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower().strip())
+    
+    # create tokens out of the text
+    tokens = word_tokenize(text)
+    
+    # init stemmer, lemmatizer and stop words
+    stemmer = PorterStemmer()
+    lemmatizer = WordNetLemmatizer()
+    stop_words = stopwords.words("english")
+
+    # stemmatize and lemmatize the tokens
+    stemmed = [stemmer.stem(w) for w in tokens if w not in stop_words]
+    tokens = [lemmatizer.lemmatize(w) for w in stemmed]
+    
+    return tokens
 
 
 def build_model():
-    pass
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier()))
+    ])
+
+    parameters = {
+        #'vect__ngram_range': ((1, 1), (1, 2)),
+        # 'vect__max_df': (0.5, 0.75, 1.0),
+        #'vect__max_features': (None, 5000, 10000),
+        #'tfidf__use_idf': (True, False),
+        #'clf__estimator__n_estimators': [50, 100, 200],
+        #'clf__estimator__min_samples_split': [2, 3, 4],
+        'clf__estimator__min_samples_split': [2],
+    }
+
+    cv = GridSearchCV(pipeline, param_grid=parameters)
+
+    return cv
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    Y_pred = model.predict(X_test)
+    accuracy_list = []
+    precision_list = []
+    recall_list = []
+    for idx, category in enumerate(category_names):
+        # classification report per category
+        report = classification_report(Y_test.iloc[:, idx], Y_pred[:, idx], output_dict=True)
+        print(report)
+        # accuracy, precision and recall for the category
+        accuracy_list.append(accuracy_score(Y_test.iloc[:, idx], Y_pred[:, idx]))
+        precision_list.append(report["macro avg"]["precision"])
+        recall_list.append(report["macro avg"]["recall"])
+    
+    # print overall metrics
+    print("Overall Accuracy: {}".format(np.mean(np.array(accuracy_list))))
+    print("Overall Precision: {}".format(np.mean(np.array(precision_list))))
+    print("Overall Recall: {}".format(np.mean(np.array(recall_list))))
 
 
 def save_model(model, model_filepath):
-    pass
+    """
+    Save model as a pickle file.
+
+    :param model: Estimator model to be saved
+    :param model_filepath: Total filepath where the model will be saved
+    """
+    pickle.dump(model, open(model_filepath, "wb"))
 
 
 def main():
