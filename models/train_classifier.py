bf8b786b1fdb19e9aa6d74126e24496afc0c16d2
Franzke <franzk1@emea.corpdir.net>
2020-04-15 11:28:34 +0200

    Make gridsearch verbose and print results pretty

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,4 +1,5 @@
 import pickle
+import pprint
 import re
 import sys
 
@@ -82,11 +83,11 @@ def build_model():
         'vect__max_df': (0.5, 0.75, 1.0),
         'vect__max_features': (None, 5000, 10000),
         'tfidf__use_idf': (True, False),
-        'clf__estimator__n_estimators': [50, 100, 200],
+        'clf__estimator__n_estimators': [50], 100, 200],
         'clf__estimator__min_samples_split': [2, 3, 4],
     }
 
-    cv = GridSearchCV(pipeline, param_grid=parameters)
+    cv = GridSearchCV(pipeline, param_grid=parameters, verbose=4)
 
     return cv
 
@@ -105,7 +106,7 @@ def evaluate_model(model, X_test, Y_test, category_names):
         recall_list.append(report["weighted avg"]["recall"])
         # print report for each category
         print(category)
-        print(report)
+        pprint(report)
         
     # print overall metrics
     print("Average accuracy: {}".format(np.mean(np.array(accuracy_list))))
